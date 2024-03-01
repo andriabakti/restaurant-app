2765ec3c549a9468a7b4c3f707435fb56feb2625
Andria Bakti Mahendra <andriabaktimahendra@gmail.com>
2024-03-02 00:05:22 +0700

    setup(config): setting up config for JWT

--- apps/order-service/src/configs/base-config.module.ts
+++ apps/order-service/src/configs/base-config.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_KEY,
+      signOptions: {
+        expiresIn: process.env.JWT_TTL,
+      },
     }),
   ],
 })
