973aea3b847ec76157500224f09279764de92611
Andria Bakti Mahendra <andriabaktimahendra@gmail.com>
2024-03-02 10:38:37 +0700

    setup(kitchen/queue): setting up database & consumer for order process queue

--- apps/kitchen-service/src/app.module.ts
+++ apps/kitchen-service/src/app.module.ts
@@ -1,9 +1,28 @@
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './configs/database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      uri: process.env.RMQ_URL,
+      queues: [
+        {
+          name: 'order.process',
+          options: {
+            durable: true,
+          },
+        },
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/kitchen-service/src/app.service.ts
+++ apps/kitchen-service/src/app.service.ts
@@ -1,8 +1,30 @@
+import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from './configs/database/database.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly db: DatabaseService) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  @RabbitSubscribe({
+    exchange: 'ORDER_EXCHANGE',
+    routingKey: '',
+    queue: 'order.process',
+  })
+  public async updateOrderStatus(msg: { orderId: string }) {
+    return await this.db.order.update({
+      where: {
+        id: msg.orderId,
+      },
+      data: {
+        status: 'PROCESSED',
+      },
+      select: {
+        status: true,
+      },
+    });
   }
 }
