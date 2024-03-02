bae61a65bcc6ae817309684f4d8b0d1c8055afc3
Andria Bakti Mahendra <andriabaktimahendra@gmail.com>
2024-03-03 03:01:39 +0700

    feat(notif/email): add service to send email

--- apps/notif-service/src/app.service.ts
+++ apps/notif-service/src/app.service.ts
@@ -1,8 +1,15 @@
 import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from './configs/database/database.service';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class AppService {
+  constructor(
+    private readonly db: DatabaseService,
+    private readonly mailerSvc: MailerService,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
   }
@@ -12,7 +19,37 @@ export class AppService {
     routingKey: '',
     queue: 'order.confirmation',
   })
-  public async sendConfirmEmail(msg: { userEmail: string }) {
-    // send email here
+  public async sendConfirmEmail(msg: { orderId: string; userEmail: string }) {
+    const result = await this.db.orderItem.findMany({
+      where: {
+        orderId: msg.orderId,
+      },
+      include: {
+        food: true,
+      },
+    });
+
+    const itemList = result.map((el: any) => {
+      return {
+        name: el.food.name,
+        price: el.food.price,
+        quantity: el.quantity,
+        subTotal: el.food.price * el.quantity,
+      };
+    });
+    const total = itemList.reduce((acc: any, el: any) => {
+      return acc + el.subTotal;
+    }, 0);
+
+    const emailData = {
+      to: msg.userEmail,
+      subject: 'Order Confirmation',
+      template: 'order-confirmation.html',
+      context: { itemList, total },
+    };
+    await this.mailerSvc
+      .sendMail(emailData)
+      .catch((err: any) => console.log(err, '< err sent email'));
+    return;
   }
 }
